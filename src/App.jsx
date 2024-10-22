8cac4195fd5ba8e64367302bc995a5eb0648a88f
Dulshad <57455107+Dulshad@users.noreply.github.com>
2024-10-22 18:01:42 +0200

    added new project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar/Navbar"
 import Hero from "./components/hero/Hero"
+import Card from "./components/card/Card"
 
 export default function App() {
     return (
         <>
             <Navbar />
             <Hero />
+            <Card />
         </>
     )
 }
