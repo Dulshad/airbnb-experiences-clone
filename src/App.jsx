b6a4ed7822655037d1ad67649188239481b9ee5f
Dulshad <57455107+Dulshad@users.noreply.github.com>
2024-10-24 10:01:19 +0200

    using data.js to make cards and make it more responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import Navbar from "./components/navbar/Navbar"
 import Hero from "./components/hero/Hero"
 import Card from "./components/card/Card"
+import data from "./data.js"
 
 export default function App() {
+
+    const cards = data.map((item) => {
+        return (
+            <Card
+                key={item.id}
+                img={`./src/images/${item.coverImg}`}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
+
     return (
         <>
-            <Navbar />
-            <Hero />
-            <Card
-              img="./src/images/katie-zaferes.png"
-              rating="5.0"
-              reviewCount={6}
-              country="Netherlands"
-              title="Life Lessons with Katie Zaferes"
-              price={136}
-            />
+            <Navbar/>
+            <Hero/>
+            <section className='cards-container'>
+                {cards}
+            </section>
         </>
     )
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,19 +1,17 @@
 import starIcon from '../../images/star.png'
 import styles from './Card.module.css'
 
-const Card = ({img, rating, reviewCount, country, title, price}) => {
+const Card = ({img, rating, reviewCount, location, title, price}) => {
     return (
-        <section className={styles['cards-container']}>
             <div className={styles['card-container']}>
                 <img className={styles['card-img']} src={img} alt="picture of katie zaferes in swim suit"/>
                 <div className={styles['card-rating']}>
                     <img className={styles['card-rating-icon']} src={starIcon} alt="icon of star"/>
-                    <p>{rating} <span className={styles['card-light-text']}>({reviewCount}) - {country}</span></p>
+                    <p>{rating} <span className={styles['card-light-text']}>({reviewCount}) - {location}</span></p>
                 </div>
                 <p>{title}</p>
                 <p><span className={styles['card-bold-text']}>From ${price} </span> / person</p>
             </div>
-        </section>
     )
 }
 
